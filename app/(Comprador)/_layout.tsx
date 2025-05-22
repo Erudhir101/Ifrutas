b9508ad8b9fde435a49000bd5374acf732f90efe
Priscila Nunes <priscilapereiranunes@gmail.com>
2025-05-22 18:54:57 -0300

    Inclui componente NavigationBar

--- app/(Comprador)/_layout.tsx
+++ app/(Comprador)/_layout.tsx
@@ -3,16 +3,32 @@ import "react-native-reanimated";
 import { ThemeProvider } from "../../components/ThemeContext.js";
 import { AuthProvider } from "../../hooks/AuthContext";
 import { StatusBar } from "expo-status-bar";
+import NavigationBar from "../../components/NavigationBar";
+import { View, StyleSheet } from "react-native";
 
 export default function layoutComprador() {
   return (
     <AuthProvider>
       <ThemeProvider>
         <StatusBar style="dark" />
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="homeComprador" />
-        </Stack>
+        <View style={styles.container}>
+          <View style={styles.content}>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="homeComprador" />
+            </Stack>
+          </View>
+          <NavigationBar activeTab="Inicio" />
+        </View>
       </ThemeProvider>
     </AuthProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
+});
